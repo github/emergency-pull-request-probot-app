4c931d797c07f530e59a6ea547488b5ba23a47d2
Rob Anderson <robandpdx@github.com>
2022-02-16 21:07:26 -0800

    add approval to PR and merge it

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+const axios = require('axios');
+
 /**
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
   app.log("Yay! The app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    return context.octokit.issues.createComment(
-      context.issue({ body: "Hello, World!" })
-    );
+  app.on("pull_request.labeled", async (context) => {
+    if (context.payload.label.name == "emergency") {
+      app.log("Emergency label detected");
+      context.octokit.pulls.createReview(
+        context.pullRequest({ event: "APPROVE", body: "Emergency PR approved" })
+      );
+      return axios({
+        method: 'put',
+        url: `${context.payload.pull_request.url}/merge`,
+        auth: {
+          username: process.env.GITHUB_USER,
+          password: process.env.GITHUB_PAT
+        }
+      });
+    }
   });
 };
