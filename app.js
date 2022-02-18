ab9d037ba8048cbcf1eef2b06756d08d2345b994
Rob Anderson <robandpdx@github.com>
2022-02-17 20:23:09 -0800

    use new vars

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const axios = require('axios');
+const emergencyLabel = process.env.EMERGENCY_LABEL;
+const reviewBody = process.env.REVIEW_BODY;
+const auth = {
+  username: process.env.GITHUB_USER,
+  password: process.env.GITHUB_PAT
+}
 
 /**
  * @param {import('probot').Probot} app
@@ -7,18 +13,15 @@ module.exports = (app) => {
   app.log("Yay! The app was loaded!");
 
   app.on("pull_request.labeled", async (context) => {
-    if (context.payload.label.name == "emergency") {
+    if (context.payload.label.name == emergencyLabel) {
       app.log("Emergency label detected");
       context.octokit.pulls.createReview(
-        context.pullRequest({ event: "APPROVE", body: "Emergency PR approved" })
+        context.pullRequest({ event: "APPROVE", body: reviewBody })
       );
       return axios({
         method: 'put',
         url: `${context.payload.pull_request.url}/merge`,
-        auth: {
-          username: process.env.GITHUB_USER,
-          password: process.env.GITHUB_PAT
-        }
+        auth: auth
       });
     }
   });
