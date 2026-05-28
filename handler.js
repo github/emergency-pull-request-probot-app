b8bac1de100ed1dc0d493697243a88e36fa9f2d9
Rob Anderson <robandpdx@github.com>
2026-05-28 15:07:38 -0400

    GitHub API version 2026-03-10

--- handler.js
+++ handler.js
@@ -1,10 +1,20 @@
 const {
   createLambdaFunction,
   createProbot,
+  ProbotOctokit,
 } = require("@probot/adapter-aws-lambda-serverless");
 
 const appFn = require("./");
 
+const Octokit = ProbotOctokit.defaults({
+  userAgent: "emergency-pull-request-probot-app",
+  request: {
+    headers: {
+      "X-GitHub-Api-Version": "2026-03-10",
+    },
+  },
+});
+
 module.exports.webhooks = createLambdaFunction(appFn, {
-  probot: createProbot(),
+  probot: createProbot({ overrides: { Octokit } }),
 });
